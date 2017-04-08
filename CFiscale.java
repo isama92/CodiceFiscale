7e6422f21ab8bbaecb9c413aed564b96430d2e14
isama92 <isama92@gmail.com>
2017-04-08 11:17:05 +0200

    removed few personal datas

--- CFiscale.java
+++ CFiscale.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class CFiscale {
     public static void main(String[] args) throws IOException {
-        String CF = CodiceFiscale.calcola("Telesca", "Leonardo", 27, 11, 1990, 'm', "Melfi");
+        String CF = CodiceFiscale.calcola("Cognome", "Nome", 1, 1, 1990, 'm', "Roma");
         System.out.println(CF);
     }
 }
